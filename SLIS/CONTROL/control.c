2b830f5b983815c4b48f3f4449fde67bed2eb2d2
jialiang.li <80201288@qq.com>
2018-12-16 13:51:27 +0800

    修改 woke_mode_modbus_http_main，增加 modbus_http_get_send2 新的命令发送测试函数

--- SLIS/CONTROL/control.c
+++ SLIS/CONTROL/control.c
@@ -5,13 +5,11 @@
 #include "sim800c.h"
 #include "hostif.h"
 #include "string.h"
-#define Modbus_Commands_Length 16
-#define Post_Buffer_Length 4096
-#define Get_Modbus_Commands_Split_Zero_Count 8
+
 
 unsigned int post_action_send_modbus_commands_Index = 0;
+unsigned int get_send_modbus_command_working_flag = 0; //get方式正在发送modbus命令标示，该标示不为0时 post不能工作
 unsigned int modbus_get_flog = 0;
-
 unsigned char modbus_http_post_buff[Post_Buffer_Length] = {0x00};
 unsigned int modbus_http_post_len = 0;
 
@@ -19,7 +17,6 @@ unsigned int modbus_http_post_len = 0;
 void woke_mode_at_main(void)
 {
 	//初始化该模式数据
-	
     while(1)
     {
         host_receive_packet();
@@ -32,156 +29,10 @@ void woke_mode_at_main(void)
     }
 }
 
-void woke_mode_ts_main(void)
-{
-	  int res = 0,i = 0;
-	  static unsigned int check_tcp_udp_connect_stime = 0;
-	  static unsigned int heart_beat_stime = 0;
-	  static unsigned int tcp_udp_off_line_stime = 0;
-	  //初始化该模式数据
-	  check_tcp_udp_connect_stime = 0;
-	  heart_beat_stime = 0;
-	  check_tcp_udp_connect_stime = 0;
-	  while(1)
-		{
-			  res = 0;
-			  i = 0;
-	      host_receive_packet();
-		    SIM8XX_User_Receive();
-	      if(para_value.on_line_mode ==1)//永远在线模式
-		    {
-		        if(time_diff_ms(check_tcp_udp_connect_stime) > CHECK_TCP_UDP_CONNECT_TIMEOUT)
-		    		{
-							  check_tcp_udp_connect_stime = time_get_ms();
-                tcp_udp_auto_line();
-		    		    tcp_udp_off_line_stime = time_get_ms();
-		    		}
-		    }
-		    //唤醒在线模式或者按需在线模式
-		    //这两种模式要隔一段时间自动下线
-		    else if((para_value.on_line_mode == 2) || (para_value.on_line_mode == 3))
-		    {
-		        //判断自动下线时间是否到了
-		    	  if(time_diff_ms(tcp_udp_off_line_stime) > (para_value.auto_off_line_time * 1000))
-		    		{
-		    			  if(check_tcp_udp_connet() == 1)//如果tcp在线，则离线
-		    				{
-		    				    off_line();
-		    				}
-		    			  
-		    		    tcp_udp_off_line_stime = time_get_ms();
-		    		}
-		    }
-		    if(tcp_udp_rx_data_flog == 1)
-		    {
-		    	  memset(host_tx_buff, 0, HOST_TX_BUFF_LEN);
-		        res = tcp_udp_rx_data(host_tx_buff);
-		    	  usart_send_str(HOSTIF_USART, host_tx_buff, (u16)res);
-		    	  tcp_udp_rx_data_flog = 0;
-		    }
-		    if(first_tcp_udp_connect_flog == 1)
-		    {
-		    	  //发送注册包信息
-		    	  if(para_value.reg_packet_switch == 1)
-		    		{
-		    		    for(i=0; i<(para_value.reg_head_len+para_value.id_len); i++)
-		    			  {
-		    					  if(i < para_value.reg_head_len)
-		    						{
-		    						    host_tx_buff[i] = para_value.reg_head[i];
-		    						}
-		    						else
-		    						{
-		    						    host_tx_buff[i] = para_value.id[i-para_value.reg_head_len];
-		    						}
-		    				    
-		    					
-		    				}
-		    			  tcp_udp_send(host_tx_buff, (para_value.reg_head_len+para_value.id_len));
-		    		}
-		    	  
-		    	  
-		    	  //读取建立连接的时间
-		    	  tcp_udp_off_line_stime = time_get_ms();
-		    		if(para_value.heartbeat_time > 0)
-		        {
-		    			  heart_beat_stime = time_get_ms();
-		        }
-		    		
-		        first_tcp_udp_connect_flog = 0;
-		    }
-
-		    if(para_value.heartbeat_time == 0)
-		    {
-		        heart_beat_stime = 0;
-		    }
-		    else
-		    {
-		        if(heart_beat_stime == 0)
-		    		{
-		    		    heart_beat_stime = time_get_ms();
-		    		}
-		    }
-
-		    //判断心跳包建立时间，发送心跳包
-		    if((heart_beat_stime != 0) && (para_value.word_mode != 0))
-		    {
-		        if(time_diff_ms(heart_beat_stime) > (para_value.heartbeat_time * 1000))
-		    		{
-		    			
-		    			  if(check_tcp_udp_connet() != 0)
-		    				{
-		    		        for(i=0; i<(para_value.heartbeat_head_len+para_value.id_len); i++)
-		    			      {
-		    				    	  if(i < para_value.heartbeat_head_len)
-		    				    		{
-		    				    		    host_tx_buff[i] = para_value.heartbeat_head[i];
-		    				    		}
-		    				    		else
-		    				    		{
-		    				    		    host_tx_buff[i] = para_value.id[i-para_value.heartbeat_head_len];
-		    				    		}
-		    				    }
-		    			      tcp_udp_send(host_tx_buff, (para_value.heartbeat_head_len+para_value.id_len));
-		    				}
-		    				heart_beat_stime = time_get_ms();
-		    				tcp_udp_off_line_stime = time_get_ms();
-		    		}
-		    }
-				if(para_value.word_mode != WOKE_MODE_TS)
-				{
-				    break;
-				}
-		
-		}
-	
-
-		
-		
-}
-
-void woke_mode_http_main(void)
-{
-	  //初始化该模式数据
-	
-    while(1)
-		{
-	      host_receive_packet();
-		    SIM8XX_User_Receive();
-			
-			
-			  if(para_value.word_mode != WOKE_MODE_HTTP)
-				{
-				    break;
-				}
-		}
-}
-
-
 void modbus_http_get_send(u8 *data, u16 len)
 {
-    unsigned int wait_modbus_get_stime = 0;
-    u16 i=0;
+  unsigned int wait_modbus_get_stime = 0;
+  u16 i=0;
 	u16 count = 0;
 	if((len%8) == 0)
 	{
@@ -200,6 +51,7 @@ void modbus_http_get_send(u8 *data, u16 len)
         //清除接收的数据
         memset(host_rx_buff, 0, host_rx_len);
         host_rx_len = 0;
+				delay_ms(100);//延迟100ms退出，防止设备返回数据污染POST数据缓存区
 	}
 }
 void modbus_http_get_send2(u8 *data, u16 len)
@@ -241,11 +93,11 @@ void woke_mode_modbus_http_main(void)
 		static unsigned int modbus_cmd_interval = 0;//modbus命令发送间隔计数
 		unsigned int work_flag = 0;//post大周期到来标识 0未到来 1到来
 		unsigned int dealy = 0;//延迟
-		u16 i = 0;
-	//断电测试flag
-		u8 power_flag = 0;
+		
+	//http_connect_count 一个周期内 http 连接次数
+	u8 http_connect_count = 0;
     
-		unsigned int timer = 0;
+		//unsigned int timer = 0;
     //初始化该模式数据
 		work_interval = time_get_ms();
     post_action_send_modbus_commands_Index = 0;
@@ -259,86 +111,65 @@ void woke_mode_modbus_http_main(void)
 			
 			//1 检测POST时间设置 大于0则进入工作模式
 			if(para_value.modbus_qry_post_time > 0){
+
 				if(time_diff_ms(work_interval) > (para_value.modbus_qry_post_time * 1000)){ //检测POST时间间隔的到来 大周期
-					//DEBUG("\r\nwork is coming \r\n");
+					//统计周期内http连接次数，确保在完成必要的连接次数后关闭 http 连接承载
+					 http_connect_count = (para_value.link1.connect_type==0?1:0)
+					+
+					(para_value.link2.connect_type==0?1:0);
+					//DEBUG("http count %d \r\n",http_connect_count);
+					//进入大周期后，先清理POST缓冲区，防止数据污染
+					modbus_http_post_len = 0;
+					memset(modbus_http_post_buff, 0, Post_Buffer_Length);
+					
+					work_flag = 1;
 					//POST ACTION 发送串口查询指令开始
 					modbus_cmd_interval = time_get_ms(); //初始化modbus 命令间隔计数
 					work_interval = time_get_ms(); //重置POST时间间隔计数器，用于不停检测下次大周期是否到来
-					work_flag = 1;
-					
-					/** 断电测试
-					if(power_flag){
-						power_flag = 0;
-						DEBUG("断电\r\n");
-						SIM_PWR_Close();
-						para_value.modbus_qry_post_time=300;
-					}
-					else{
-						power_flag = 1;
-						DEBUG("上电\r\n");
-						SIM_PWR_Open();
-						para_value.modbus_qry_post_time=30;
-					}
-					**/
-					/**
-					char 打印测试
-					**/
-					DEBUG("char -1->%d\r\n",((char)-1));
-					DEBUG("char -2->%d\r\n",((char)-2));
-					DEBUG("char -3->%d\r\n",((char)-3));
-					DEBUG("char -4->%d\r\n",((char)-4));
-					DEBUG("char -5->%d\r\n",((char)-5));
-					DEBUG("char -6->%d\r\n",((char)-6));
-					DEBUG("char -7->%d\r\n",((char)-7));
-					DEBUG("char -8->%d\r\n",((char)-8));
-					continue;
 				}
-				if(work_flag && time_diff_ms(modbus_cmd_interval) > para_value.modbus_qry_cmd_time){//检测大周期到来且发送modbus命令间隔时间的到来
+				//检测大周期到来且发送modbus命令间隔时间的到来
+				if(work_flag && time_diff_ms(modbus_cmd_interval) > para_value.modbus_qry_cmd_time){
 					//DEBUG("\r\ncmd index %d\r\n",post_action_send_modbus_commands_Index);
-					if(Modbus_Commands_Length == post_action_send_modbus_commands_Index){//如果命令索引超过总数，命令索引归0,调用HTTP POST
+					//如果命令索引超过总数，命令索引归0,调用HTTP POST
+					if(Modbus_Commands_Length == post_action_send_modbus_commands_Index){
 						post_action_send_modbus_commands_Index = 0;
 						work_flag = 0;
-						DEBUG("\r\nhttp post data %d server1 %d server2 %d \r\n",modbus_http_post_len,para_value.link1.connect_type,para_value.link2.connect_type);
+						//DEBUG("\r\nhttp post data %d server1 %d server2 %d \r\n",modbus_http_post_len,para_value.link1.connect_type,para_value.link2.connect_type);
 						//判断是否需要执行HTTP POST请求
-						//sprintf("");
 						if(modbus_http_post_len){//接收modbus查询命令返回的数据不为空
-							if(para_value.link1.connect_type == 0)//TCP连接有效，执行HTTP POST
-							{
-								http_post(para_value.link1, para_value.modbus_http_post_para, modbus_http_post_buff, modbus_http_post_len);
+							
+								DEBUG("\r\nPOST Code:%d\r\n",http_post(para_value.link1, para_value.modbus_http_post_para, modbus_http_post_buff, modbus_http_post_len,http_connect_count));
 								/**
 								清理HTTP POST 缓冲区，不可使用HTTP GET中的（清理HTTP POST 缓冲区）操作替换。
 								因为HTTP GET 行为并不一定会发生
 								**/
 								modbus_http_post_len = 0;
 								memset(modbus_http_post_buff, 0, Post_Buffer_Length);
-								DEBUG("\r\nPOST\r\n");
 								dealy = time_get_ms();
-								while(time_diff_ms(dealy) < 5000);
-							}
+								while(time_diff_ms(dealy) < 3000);
+						}
 							//清理接收的modbus查询命令返回的数据
 							modbus_http_post_len = 0;
 							memset(modbus_http_post_buff, 0, Post_Buffer_Length);
-						}
+						
 						//执行get请求
-						if(para_value.link2.connect_type == 0)//TCP连接
-        		{
-							if(http_get(para_value.link2, para_value.modbus_http_get_para, para_value.modbus_qry_get_para.buff) == 1)
+						
+							modbus_get_flog = 1;
+							int code = http_get(para_value.link2, para_value.modbus_http_get_para, para_value.modbus_qry_get_para.buff);
+							DEBUG("\r\nGET Code:%d\r\n",code);
+							if(!code)
 							{
-								DEBUG("\r\nGET\r\n");
+								DEBUG("\r\nhttp_get_rx_len:%d\r\n",http_get_rx_len);
 								modbus_http_get_send(http_get_buff, http_get_rx_len);
-								/**
-								GET 获取指令发送后，延迟MODBUS_GET_RX_TIMEOUT后清理HTTP POST 数据缓冲区。
-								在此之前存在GET到的指令发送后，设备反馈数据污染HTTP POST数据缓冲区的情况。
-								**/
-								dealy = time_get_ms();
-								while(time_diff_ms(dealy) < MODBUS_GET_RX_TIMEOUT);
-								modbus_http_post_len = 0;
-								memset(modbus_http_post_buff, 0, Post_Buffer_Length);
 							}
-						}
+							modbus_get_flog = 0;
+						
 					}
 					else{
 						if(para_value.modbus_qry_post_para[post_action_send_modbus_commands_Index].len > 0){//命令长度有效发送命令
+							//if(0 == post_action_send_modbus_commands_Index){//在发送第一条Query Modbus命令时延迟100ms,防止Get Modubs命令返回值与该命令返回值重叠
+							//	delay_ms(200);
+							//}
 							usart_send_str(HOSTIF_USART, 
 								para_value.modbus_qry_post_para[post_action_send_modbus_commands_Index].buff, 
 								para_value.modbus_qry_post_para[post_action_send_modbus_commands_Index].len);
@@ -350,10 +181,10 @@ void woke_mode_modbus_http_main(void)
 					}
 				}
 			}
-		
 			if(para_value.word_mode != WOKE_MODE_MODBUS_HTTP)
 			{
 				break;
 			}
 		}
 }
+
--- SLIS/CONTROL/control.h
+++ SLIS/CONTROL/control.h
@@ -5,7 +5,9 @@
 
 
 
-
+#define Modbus_Commands_Length 16
+#define Post_Buffer_Length 4096
+#define Get_Modbus_Commands_Split_Zero_Count 8
 #define SIM800C_RX_TIMEOUT                         10
 #define CHECK_TCP_UDP_CONNECT_TIMEOUT              10000
 #define MODBUS_GET_RX_TIMEOUT                      200
@@ -13,14 +15,11 @@
 extern unsigned int modbus_post_count;
 extern unsigned int modbus_get_flog;
 
-extern unsigned char modbus_http_post_buff[4096];
+extern unsigned char modbus_http_post_buff[Post_Buffer_Length];
 extern unsigned int modbus_http_post_len;
 
 
 extern void woke_mode_at_main(void); 			
-extern void woke_mode_ts_main(void);
-
-extern void woke_mode_http_main(void);
 extern void woke_mode_modbus_http_main(void);
 
 #endif
